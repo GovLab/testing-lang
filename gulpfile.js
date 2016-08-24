a8814ba70f368056f9592d7115ef351f2d1666af
mocxd <mark.adkins.hastings@gmail.com>
2016-08-24 17:32:30 -0400

    http-enabled gulp-data for nunjucks templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var del             = require('del'),
-    gulp            = require('gulp'),
-    sass            = require('gulp-sass'),
-    data            = require('gulp-data'),
-    shell           = require('gulp-shell'),
-    uglify          = require('gulp-uglify'),
-    ghPages         = require('gulp-gh-pages'),
-    imagemin        = require('gulp-imagemin'),
-    sourcemaps      = require('gulp-sourcemaps'),
-    browserSync     = require('browser-sync'),
-    runSequence     = require('run-sequence').use(gulp),
-    nunjucksRender  = require('gulp-nunjucks-render');
+gulp            = require('gulp'),
+sass            = require('gulp-sass'),
+data            = require('gulp-data'),
+shell           = require('gulp-shell'),
+uglify          = require('gulp-uglify'),
+ghPages         = require('gulp-gh-pages'),
+imagemin        = require('gulp-imagemin'),
+sourcemaps      = require('gulp-sourcemaps'),
+browserSync     = require('browser-sync'),
+runSequence     = require('run-sequence').use(gulp),
+nunjucksRender  = require('gulp-nunjucks-render'),
+request         = require('request');
+
+var nunjucksEnv = function (env) {
+  env.addGlobal('getContext', function() {
+    return this.ctx;
+  });
+}
 
 // Clean Dist
 gulp.task('clean', function () {
@@ -29,33 +36,33 @@ gulp.task('browserSync', function () {
 gulp.task('sass', function () {
   // Gets all files ending with .scss in source/sass
   return gulp.src('source/sass/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'compressed' })).on('error', sass.logError)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('public/styles'))
-    .pipe(browserSync.reload({ stream: true }));
+  .pipe(sourcemaps.init())
+  .pipe(sass({ outputStyle: 'compressed' })).on('error', sass.logError)
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('public/styles'))
+  .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task('vendor', function () {
   return gulp.src('source/static/vendor/**/*')
-    .pipe(gulp.dest('public/vendor'));
+  .pipe(gulp.dest('public/vendor'));
 });
 
 gulp.task('image', function () {
   return gulp.src('source/static/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('public/images'));
+  .pipe(imagemin())
+  .pipe(gulp.dest('public/images'));
 });
 
 gulp.task('js', function () {
   return gulp.src('source/static/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('public'));
+  .pipe(uglify())
+  .pipe(gulp.dest('public'));
 });
 
 gulp.task('json', function () {
   return gulp.src('source/static/**/*.json')
-    .pipe(gulp.dest('public'));
+  .pipe(gulp.dest('public'));
 });
 // Nunjucks
 // gulp.task('nunjucks', function () {
@@ -72,24 +79,37 @@ gulp.task('json', function () {
 
 gulp.task('nunjucks', function() {
   return gulp.src('source/templates/**/*.+(html|nunjucks)')
-    // Adding data to Nunjucks
-    .pipe(data(function() {
-      return require('./source/data/data.json')
-    }))
-    .pipe(nunjucksRender({
-      path: ['source/templates']
-    }))
-    .pipe(gulp.dest('public'))
-    .pipe(browserSync.reload({ stream: true }));
+  .pipe(data(function(file, cb) {
+
+    var httpData = '';
+    request
+    .get('https://raw.githubusercontent.com/GovLab/orgpedia-prototype/master/source/data/company.json')
+    .on('response', function (response) {
+      response.on('data', (chunk) => {
+        if (chunk) { httpData += chunk };
+      });
+      response.on('end', () => {
+        if (httpData.length) { console.log('Stream data recieved from HTTP (length)', httpData.length); }
+        else { console.log('No data received from HTTP'); }
+        cb(undefined, JSON.parse(httpData));
+      });
+    });
+
+  }))
+  .pipe(nunjucksRender({
+    path: ['source/templates'],
+    manageEnv: nunjucksEnv
+  }))
+  .pipe(gulp.dest('public'))
+  .pipe(browserSync.reload({ stream: true }));
 });
 
-      //https://raw.githubusercontent.com/GovLab/orgpedia-prototype/master/source/data/company.json
 
 gulp.task('push-gh-master', shell.task(['git push origin master']));
 
 gulp.task('push-gh-pages', function () {
   return gulp.src('public/**/*')
-    .pipe(ghPages({ force: true }));
+  .pipe(ghPages({ force: true }));
 });
 
 gulp.task('deploy', function (callback) {
@@ -99,7 +119,7 @@ gulp.task('deploy', function (callback) {
     'push-gh-master',
     'push-gh-pages',
     callback
-  );
+    );
 });
 
 gulp.task('watch', function () {
@@ -116,5 +136,6 @@ gulp.task('default', function (callback) {
     ['sass', 'js', 'json', 'image', 'nunjucks', 'vendor'],
     ['browserSync', 'watch'],
     callback
-  );
+    );
 });
+//https://raw.githubusercontent.com/GovLab/orgpedia-prototype/master/source/data/company.json
